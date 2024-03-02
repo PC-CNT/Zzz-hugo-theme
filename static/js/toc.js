34fd4c944d61c1c629fbe90b1891de0a72cce0b7
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2024-03-02 22:38:16 +0900

    minor changes

--- static/js/toc.js
+++ static/js/toc.js
@@ -1,49 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     if (document.querySelector(`article`)) {
-        const caps = document.querySelector(`article`).querySelectorAll(`h1, h2, h3, h4, h5, h6`);
-
         const options = {
             root: null,
-            rootMargin: "0px 0px -95% 0px",
+            rootMargin: "0px 0px -80% 0px",
             threshold: 0
         };
 
         const callback = (entries) => {
-            // console.log(entries);
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    change(entry.target)
+                    change(entry.target);
                 }
             });
         }
 
         const observer = new IntersectionObserver(callback, options);
 
-        caps.forEach((cap) => {
-            observer.observe(cap);
-        });
-
         const change = (element) => {
-            const toc_active_old = document.querySelector(`.focus`);
+            const toc_active_old = document.querySelector(`#TableOfContents .focus`);
             if (toc_active_old !== null) {
                 toc_active_old.classList.remove("focus");
             }
 
-            const toc_active_new = document.querySelector(`a[href="#${element.id}"]`);
+            const toc_active_new = document.querySelector(`#TableOfContents a[href="#${element.id}"]`);
             if (toc_active_new !== null) {
                 toc_active_new.classList.add("focus");
             }
         }
+
+        document.querySelector(`article`).querySelectorAll(`.heading[id]`).forEach((cap) => {
+            observer.observe(cap);
+        });
     }
 });
-
-
-// document.addEventListener("scroll", () => {
-//     document.querySelector(".focus").scrollIntoView({
-//         behavior: "smooth",
-//         block: "nearest",
-//         inline: "center"
-//     });
-// })
